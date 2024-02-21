f9e34ac2ea68d25b0798687748e26a3f5e35f6c6
Yago Andrade <contact@yagoandrade.com>
2024-02-21 02:30:19 -0300

    feat: add common, hittable and sphere entities

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
 mod color;
+mod common;
+mod hittable;
+mod hittable_list;
+mod ray;
+mod sphere;
 mod vec3;
 
 use std::io;
 
 use color::Color;
+use hittable::{HitRecord, Hittable};
+use hittable_list::HittableList;
+use ray::Ray;
+use sphere::Sphere;
+use vec3::{Point3, Vec3};
+
+fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
+    let mut rec = HitRecord::new();
+    if world.hit(r, 0.0, common::INFINITY, &mut rec) {
+        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
+    }
+
+    let unit_direction = vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+
+    // World
+
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
     println!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -16,17 +57,16 @@ fn main() {
     for i in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", i);
         for j in 0..IMAGE_WIDTH {
-            let r = j as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = i as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-            let pixel_color = Color::new(r, g, b);
+            let u = j as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = i as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+
+            let pixel_color = ray_color(&r, &world);
+
             color::write_color(&mut io::stdout(), pixel_color);
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            print!("{} {} {}\n", ir, ig, ib);
         }
     }
     eprint!("\nDone.\n");
